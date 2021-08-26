4eecb811063888950134564836f07f15ceb9da72
Chase Mack <chase.mack@yahoo.com>
2021-08-25 22:07:56 -0500

    progress

--- src/Components/Equipment.js
+++ src/Components/Equipment.js
@@ -2,16 +2,42 @@ import React from 'react';
 
 class Equipment extends React.Component {
     inventory = {
-        match: '',
-        flashlight: '',
-        key: '',
-        food: '',
-        code: '',
-        number: '',
-        combination: '',
-        bat: '',
-        crowbar: '',
-        button: ''
+        match: {
+            wrongRoom: 'There are not any matches in this room.',
+            equip: 'You strike the last match. Before the match fades you see the foyer and a darkened kitchen in the distance. The dying flame reflects off of what appears to be a flashlight by the front door.',
+            used: 'You have already used the last match.'
+        },
+        flashlight: {
+            equip: 'You enter the foyer and take the flashlight. As you turn on the light, you see the door to a garage, a set of stairs leading up, and the living room from which you just came.',
+            used: 'You already have the flashlight.'
+        },
+        key: {
+
+        },
+        food: {
+            wrongRoom: 'There is no food in here.',
+            equip: 'You eat a can of peaches. Millions of peaches. You feel better.'
+        },
+        code: {
+
+        },
+        number: {
+
+        },
+        combination: {
+
+        },
+        bat: {
+            wrongRoom: 'This room does not contain sports equipment.',
+            equip: 'You grab the baseball bat.'
+        },
+        crowbar: {
+            useNow: 'You loudly break the lock free and the basement door swings open. Was that a crashing upstairs? The owner has baseball equipment stored here.',
+            
+        },
+        button: {
+
+        }
     };
     render() {
         return {
--- src/Components/helpers.js
+++ src/Components/helpers.js
@@ -12,22 +12,6 @@ const tryLockedDoor = () => {
         locked.play();
     }, 1500);
 }
-const unarmedAttack = () => {
-    boss.health -= 1;
-    player.health -= 3;
-}
-const armedAttack = () => {
-    boss.health -= 2;
-    player.health -= 3;
-}
-const strongUnarmedAttack = () => {
-    boss.health -= 2;
-    player.health -= 2;
-}
-const strongArmedAttack = () => {
-    boss.health -= 3;
-    player.health -= 2;
-}
 const gameOver = () => {
     if (!player.alive || !boss.alive) {
         form.style.display = 'none';
