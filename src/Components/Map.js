8095f4653695503212d352e97ecf996c8d37d19d
Chase Mack <chase.mack@yahoo.com>
2022-07-05 19:19:30 -0400

    Fixed dumb style things

--- src/Components/Map.js
+++ src/Components/Map.js
@@ -1,5 +1,34 @@
 import React from 'react';
-import { MapContainer, MapItem, PlayerOnMap } from './styles/MapStyles';
+import styled from 'styled-components';
+
+const MapContainer = styled.div`
+	position: fixed;
+	top: 0;
+	right: 0;
+	grid-template-columns: auto auto auto;
+	height: 95px;
+	width: 100px;
+	border-radius: 0.5rem;
+	background: rgb(220, 190, 95);
+`;
+
+const MapItem = styled.div`
+    border: 1px solid rgb(80, 65, 25);
+	width: 32px;
+	height: 30px;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+`;
+
+const PlayerOnMap = styled.span`
+	display: block;
+	height: 10px;
+	width: 10px;
+	margin: auto;
+	background: rgb(80, 65, 25);
+	border-radius: 50%;
+`;
 
 const mapDisplay = {
     hidden: { display: 'none' },
@@ -11,55 +40,53 @@ const playerDisplay = {
     visible: { display: 'block' }
 };
 
-class Map extends React.Component {
-    render() {
-        return (
-            <MapContainer style={this.props.gameStarted ? mapDisplay.visible : mapDisplay.hidden}>
-                <div>
-                    <MapItem>
-                        <PlayerOnMap 
-                            style={this.props.currentLocation === 'up' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'foyer' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'garage' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                </div>
-                <div>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'bedroom' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'living' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'down' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                </div>
-                <div>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'office' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'kitchen' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                    <MapItem>
-                        <PlayerOnMap
-                            style={this.props.currentLocation === 'basement' ? playerDisplay.visible : playerDisplay.hidden} />
-                    </MapItem>
-                </div>
-            </MapContainer>
-        )
-    }
+const Map = () => {
+    return (
+        <MapContainer style={this.props.gameStarted ? mapDisplay.visible : mapDisplay.hidden}>
+            <div>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'up' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'foyer' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'garage' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+            </div>
+            <div>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'bedroom' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'living' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'down' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+            </div>
+            <div>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'office' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'kitchen' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+                <MapItem>
+                    <PlayerOnMap
+                        style={this.props.currentLocation === 'basement' ? playerDisplay.visible : playerDisplay.hidden} />
+                </MapItem>
+            </div>
+        </MapContainer>
+    )
 }
 
-export default Map;
+export { Map };
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import { StartButton } from './styles/MenuStyles';
+import styled from 'styled-components';
+
+const StartButton = styled.button`
+    background: rgb(220, 190, 95);
+	padding: 0.5rem;
+	height: 3rem;
+	width: 5rem;
+	margin: auto;
+	border: 2px solid rgb(80, 65, 25);
+	border-radius: 0.5rem;
+`;
 
 const btnDisplay = {
     clicked: { display: 'none' },
     notClicked: { display: 'inline-block' }
 }
 
-class Menu extends React.Component {
-    render() {
-        return (
-            <StartButton
-                style={this.props.gameStarted ? btnDisplay.clicked : btnDisplay.notClicked}
-                onClick={this.props.handleClick}>Start
-            </StartButton>
-        )
-    }
+const Menu = () => {
+    return (
+        <StartButton
+            style={this.props.gameStarted ? btnDisplay.clicked : btnDisplay.notClicked}
+            onClick={this.props.handleClick}>Start
+        </StartButton>
+    )
 }
 
-export default Menu;
+export { Menu };
